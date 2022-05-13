ef8db1264978b3fe64a7abfd40470e3cd587eb13
Atharva Pardeshi <atharva.exe@gmail.com>
2022-05-13 11:31:05 +0530

    fix props

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,20 +1,16 @@
 import React from "react";
 import "./Track.css";
 
-let isRemoval = true;
-
 class Track extends React.Component {
-  renderAction() {
-    return <button class="Track-action">{isRemoval ? "-" : "+"}</button>;
-  }
+  renderAction() {}
+
   render() {
     return (
       <div className="Track">
         <div className="Track-information">
-          <h3>{props.name}</h3>
-          <p>{`${props.artist} | ${props.album}`}</p>
+          <h3>{}</h3>
+          <p>{}</p>
         </div>
-        {this.renderAction()}
       </div>
     );
   }
