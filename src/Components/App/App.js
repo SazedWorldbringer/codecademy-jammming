746835cd4d58b449008b8470ecf5ea86bb8285fe
Atharva Pardeshi <atharva.exe@gmail.com>
2022-05-14 17:59:48 +0530

    fix binding this to addTrack method and pass addTrack method as onAdd to SearchResults component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -20,7 +20,18 @@ class App extends React.Component {
         { name: "name3", artist: "artist3", album: "album3", id: 3 },
       ],
     };
+    this.addTrack = this.addTrack.bind(this);
   }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find((savedTrack) => savedTrack.id === track.id)) {
+      return;
+    }
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks });
+  }
+
   render() {
     return (
       <div>
@@ -30,7 +41,10 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
+            <SearchResults
+              searchResults={this.state.searchResults}
+              onAdd={this.addTrack}
+            />
             <Playlist
               playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
