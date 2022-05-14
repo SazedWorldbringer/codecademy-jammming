51d3d5b7dce6abfbc79da0a7a7182215d6989ba4
Atharva Pardeshi <atharva.exe@gmail.com>
2022-05-14 18:16:29 +0530

    fix ids

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -15,9 +15,9 @@ class App extends React.Component {
       ],
       playlistName: "New Playlist",
       playlistTracks: [
-        { name: "name1", artist: "artist1", album: "album1", id: 1 },
-        { name: "name2", artist: "artist2", album: "album2", id: 2 },
-        { name: "name3", artist: "artist3", album: "album3", id: 3 },
+        { name: "name1", artist: "artist1", album: "album1", id: 4 },
+        { name: "name2", artist: "artist2", album: "album2", id: 5 },
+        { name: "name3", artist: "artist3", album: "album3", id: 6 },
       ],
     };
     this.addTrack = this.addTrack.bind(this);
