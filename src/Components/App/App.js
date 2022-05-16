2d1ac9d431c6ce73ce0aa74ecf98ecad515975d3
Atharva Pardeshi <atharva.exe@gmail.com>
2022-05-16 12:45:43 +0530

    write updatePlaylistName method which sets a new state for playlistName, pass it as onNameChange to Playlist component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -22,6 +22,7 @@ class App extends React.Component {
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -38,6 +39,10 @@ class App extends React.Component {
     tracks = tracks.filter((currentTrack) => currentTrack.id !== track.id); // if the current track id is not equal to the given track id, then the current track stays
 
     this.setState({ playlistTracks: tracks });
+  }
+
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name });
   }
 
   render() {
@@ -57,6 +62,7 @@ class App extends React.Component {
               playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName}
             />
           </div>
         </div>
