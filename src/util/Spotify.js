c595fca82412930270d89b9ddb68805609246a4f
Atharva Pardeshi <atharva.exe@gmail.com>
2022-05-19 11:57:24 +0530

    write getAccessToken method, check if we already have accessToken and return it if we do

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,12 @@
 let accessToken;
 
-const Spotify = {};
+const Spotify = {
+  getAccessToken() {
+    // check if we already have the access token
+    if (accessToken) {
+      return accessToken;
+    }
+  },
+};
 
 export default Spotify;
